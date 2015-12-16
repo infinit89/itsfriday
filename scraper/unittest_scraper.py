f303d0515a0e2b6bded25264a3399f60a250e83f
firefiesta <analisis.jjsaezlopez@gmail.com>
2015-12-16 15:27:37 +0100

    agregado testing unitario a scraper

--- scraper/unittest_scraper.py
+++ scraper/unittest_scraper.py
@@ -1,36 +1,38 @@
-import unittest,json,urllib2;
+import unittest, json, urllib2;
 
 class testing(unittest.TestCase):
 
+    # Init
     def setUp(self):
         self.API_KEY_BING = 'XJ4NbYb5R6ui4CL2WbfDcnBtil91K/TwhpNVNzjZW+A';
         self.json = '../config.json';
 
-    # Comprueba apertura de json
+    # Check open json file
     def test_open_json(self):
         try:
              with open(self.configJson) as configJson:
                 json_data = json.load(configJson);
         except:
             # Error Open File
-            self.assertFalse(False);
+            self.assertFalse(False,'Error open json file');
         else:
             # No error
-            self.assertTrue(True);
+            self.assertTrue(True,'OK open json file');
 
-
+    # Check Variables json file
     def test_json_init_variables(self):
         try:
             with open(self.configJson) as configJson:
                 json_data = json.load(configJson);
 
-                self.assertIs(type(json_data['mongodb']['server']), str);
-                self.assertIs(type(json_data['mongodb']['port']), int);
-                self.assertIs(type(json_data['mongodb']['db']), str);
-                self.assertIs(type(json_data['mongodb']['memes_collection']), str);
+                self.assertIs(type(json_data['mongodb']['server']), str, 'Server == str');
+                self.assertIs(type(json_data['mongodb']['port']), int, 'Port == int');
+                self.assertIs(type(json_data['mongodb']['db']), str, 'DB == str');
+                self.assertIs(type(json_data['mongodb']['memes_collection']), str, 'Collection == str');
         except:
-            self.assertFalse(False);
+            self.assertFalse(False,'Error open json file');
 
+    # Check get images
     def test_getImagesBing(self):
 
         search = 'memes its friday';
@@ -48,8 +50,8 @@ class testing(unittest.TestCase):
 
         results = json.load(response);
 
-        self.assertGreater(len(results['d']['results']), 0);
+        self.assertGreater(len(results['d']['results']), 0, 'OK, get images');
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main();
